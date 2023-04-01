6fe0caedc837ae836f4dc66befbe5818cd40935f
Elena Nikiforova <vizualnoe@gmail.com>
2023-04-01 11:09:56 +0200

    amended

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,8 +18,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && npm i && npm run build && pm2 restart ecosystem.config.js',
+      'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
